b863f91c08ea078d9b75b4b6679f1a630e85fa2c
Derek <derek@Dereks-iMac.local>
2016-05-10 11:58:41 -0400

    changes for stellar at mobile

--- wp-content/themes/gandco/footer.php
+++ wp-content/themes/gandco/footer.php
@@ -129,7 +129,35 @@
 <?php wp_footer(); ?>
 
 <script type="text/javascript">
-	$.stellar();
+$(document).ready(function() {
+		react_to_window();
+});
+
+//      only activate stellar for window widths above or equal to 1024
+var stellarActivated = false;
+
+$(window).resize(function() {
+		react_to_window();
+});
+
+function react_to_window() {
+		if ($(window).width() <= 700) {
+				if (stellarActivated == true) {
+						$(window).data('plugin_stellar').destroy();
+						stellarActivated = false;
+				}
+		} else {
+				if (stellarActivated == false) {
+
+						$.stellar({
+							 horizontalScrolling: false
+					 });
+
+						$(window).data('plugin_stellar').init();
+						stellarActivated = true;
+				}
+		}
+}
 </script>
 
 </body>
