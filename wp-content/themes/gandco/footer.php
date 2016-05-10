2ac3e7de295e7d7984d165f214294cb764b8b4ec
Derek <derek@Dereks-iMac.local>
2016-05-10 18:42:00 -0400

    ie fix for header

--- wp-content/themes/gandco/footer.php
+++ wp-content/themes/gandco/footer.php
@@ -129,15 +129,31 @@
 <?php wp_footer(); ?>
 
 <script type="text/javascript">
+
+var isOpera = !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+    // Opera 8.0+ (UA detection to detect Blink/v8-powered Opera)
+    var isFirefox = typeof InstallTrigger !== 'undefined';   // Firefox 1.0+
+    var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
+    // At least Safari 3+: "[object HTMLElementConstructor]"
+    var isChrome = !!window.chrome && !isOpera;              // Chrome 1+
+    var isIE = /*@cc_on!@*/false || !!document.documentMode; // At least IE6
+
 $(document).ready(function() {
-		react_to_window();
+	if(!isSafari || !isIE)
+{
+    react_to_window();
+}
+
 });
 
 //      only activate stellar for window widths above or equal to 1024
 var stellarActivated = false;
 
 $(window).resize(function() {
+	if(!isSafari || !isIE)
+{
 		react_to_window();
+}
 });
 
 function react_to_window() {
